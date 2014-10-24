5f8eb8737ec457366eee77db320cbf39219e98b2
Florian Salbrechter <florian.salbrechter@gmail.com>
2014-10-24 21:16:51 +0200

    Added setting activity with Floating Action Button.

--- src/Onebutton/app/src/main/java/com/onebutton/SettingsActivity.java
+++ src/Onebutton/app/src/main/java/com/onebutton/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.onebutton;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,16 +10,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class SettingsActivity extends Activity {
+    public static final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +73,8 @@ public class SettingsActivity extends Activity {
 
         private Button mSearchButton;
         private EditText mZipCodeButton;
+        private ListView mServiceProviders;
+        private ArrayList<String> mServiceId;
 
         public PlaceholderFragment() {
         }
@@ -74,6 +88,13 @@ public class SettingsActivity extends Activity {
 
             mSearchButton = (Button) rootView.findViewById(R.id.search_button);
             mZipCodeButton = (EditText) rootView.findViewById(R.id.zip_code_text);
+            mServiceProviders = (ListView) rootView.findViewById(R.id.service_provider_list_view);
+
+            mServiceId = new ArrayList<String>();
+
+            SharedPreferences settings = getActivity().getSharedPreferences(PREFS_NAME, 0);
+            String zip = settings.getString("zip", "");
+            mZipCodeButton.setText(zip);
 
             mSearchButton.setOnClickListener(new View.OnClickListener() {
 
@@ -81,7 +102,7 @@ public class SettingsActivity extends Activity {
                 public void onClick(View v) {
 
 
-                    String zipCode  = mZipCodeButton.getText().toString();
+                    String zipCode = mZipCodeButton.getText().toString();
 
                     String url = "http://mobilelistings.tvguide.com/Listingsweb/ws/rest/serviceproviders/zipcode/" +
                             zipCode + "?formattype=json";
@@ -91,7 +112,36 @@ public class SettingsActivity extends Activity {
                             new Response.Listener<String>() {
                                 @Override
                                 public void onResponse(String response) {
-                                    Log.v("Settings", "Response");
+                                    Log.v("Settings", "Response" + response);
+
+                                    ArrayList<String> items = new ArrayList<String>();
+                                    // Convert the string into json object.
+                                    JSONArray serviceproviders = null;
+                                    try {
+
+                                        serviceproviders = new JSONArray(response);
+                                        for (int i = 0; i < serviceproviders.length(); i++) {
+                                            JSONObject currentObject = serviceproviders.getJSONObject(i);
+                                            JSONArray devices = currentObject.getJSONArray("Devices");
+                                            for (int j = 0; j < devices.length(); j++) {
+                                                JSONObject device = devices.getJSONObject(j);
+                                                String entry = currentObject.getString("Type") + " - " + currentObject.getString("Name");
+                                                entry += device.getString("DeviceName");
+                                                items.add(entry);
+                                                mServiceId.add(currentObject.getString("Id") + "." + device.getString("DeviceFlag"));
+                                            }
+                                        }
+
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    ArrayAdapter<String> itemsAdapter =
+                                            new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+                                    mServiceProviders.setAdapter(itemsAdapter);
+
+
                                 }
                             }, new Response.ErrorListener() {
                         @Override
@@ -103,6 +153,25 @@ public class SettingsActivity extends Activity {
                     RequestQueueSingleton.getInstance(getActivity()).addToRequestQueue(stringRequest);
                 }
             });
+
+
+
+            mServiceProviders.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    SharedPreferences settings = getActivity().getSharedPreferences(PREFS_NAME, 0);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString("serviceProviderAndDevice", mServiceId.get(position));
+                    editor.putString("zip", mZipCodeButton.getText().toString());
+
+                    // Commit the edits!
+                    editor.commit();
+
+                    Toast.makeText(getActivity(), mServiceId.get(position), Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+                }
+            });
+
             return rootView;
         }
     }
